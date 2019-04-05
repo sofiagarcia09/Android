3a5a0625e512a1f1492d0992118e46dcfe0ad3cd
sofiagarcia09 <47133963+sofiagarcia09@users.noreply.github.com>
2019-04-04 22:42:52 -0600

    Add files via upload

---
+++ HolaMundo/app/src/main/java/itz/edu/holamundo/MainActivity.java
@@ -0,0 +1,16 @@
+package itz.edu.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Hola mundo",Toast.LENGTH_LONG).show();
+    }
+}
